34938943b3daf8d743f87d6dc96f27e027273643
S <picotech@pm.me>
2022-03-08 18:02:30 -0500

    submit sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (integer1, integer2) {
+    let sum = 0;
 
+    if (integer1 < 0 || integer2 < 0) {
+        return "ERROR";
+    } else if (Number.isInteger(integer1) == false || Number.isInteger(integer2) == false) {
+        return "ERROR";
+    }
+
+    if (integer1 < integer2) {
+        for (i = integer1; i <= integer2; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = integer2; i <= integer1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
