203bcf0ef7fa647182b349456165e6bda63463dc
S <picotech@pm.me>
2022-03-08 16:12:21 -0500

    submit reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let stringReverse = "";
 
-};
+    for (let i = 1; i < string.length + 1; i++) {
+        let currentLetter = string.length - i;
+        stringReverse += string[currentLetter];
+    }
+    return stringReverse;
+}
+
 
 // Do not edit below this line
 module.exports = reverseString;
