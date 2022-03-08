d9509f8f9ba3d55fef22efbb9537f5eb38c1db48
S <picotech@pm.me>
2022-03-08 17:06:16 -0500

    submit removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+    // iterate through arguments starting at position 1 (0th is array)
+    for (let i = 1; i < arguments.length; i++) {
+        // iterate through array comparing with ith argument (2 to go through total)
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === arguments[i]) {
+                // delete ith element (same value as element)
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
