d68661fc053a3ae0d850c3d14b74a72dd2b64900
S <picotech@pm.me>
2022-03-07 23:49:47 -0500

    submit repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let currentString = string;
 
+    if (num === 0) {
+        return "";
+    } else if (num < 1) {
+        return "ERROR";
+    }
+    for (let i = 1; i < num; i++) {
+        currentString = currentString + string;
+    }
+    return currentString;
 };
 
 // Do not edit below this line
