116a06f91c99fa5429a2549055894b37d6e7f6c2
S <picotech@pm.me>
2022-03-08 22:46:58 -0500

    submit calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let total = Math.pow(num1, num2);
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  let result = 1;
+  while (num > 1) {
+    result *= num;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
