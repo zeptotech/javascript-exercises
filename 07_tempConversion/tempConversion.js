8d17788ed7349d8af523e066b8e4cbd626757747
S <picotech@pm.me>
2022-03-08 22:29:59 -0500

    submit tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (input) {
+  let output;
+  output = (input - 32) * (5 / 9);
+  output = output.toFixed(1);
+  output = parseFloat(output);
+  return output;
 };
 
-const ctof = function() {
-
+const ctof = function (input) {
+  let output;
+  output = input * (9 / 5) + 32;
+  output = output.toFixed(1);
+  output = parseFloat(output);
+  return output;
 };
 
 // Do not edit below this line
