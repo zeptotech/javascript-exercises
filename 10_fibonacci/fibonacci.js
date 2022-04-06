9f1a0a955fe86abc7c0826b7b8d555afe04e77e0
S <picotech@pm.me>
2022-04-06 19:17:33 -0400

    fibonacci submit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num < 0) {
+        return "OOPS";
+    }
 
+    num = Number(num);
+
+    let array = [1, 1]
+
+    for (let i = 1; i < num - 1; i++) {
+        array.push(array[i] + array[i - 1]);
+    }
+
+    return array[num - 1];
 };
+
+// create array
+// create loop
+// push latest calculation (sum) into array
+// return nth element of array
 
 // Do not edit below this line
 module.exports = fibonacci;
