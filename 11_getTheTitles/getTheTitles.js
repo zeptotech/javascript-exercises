3d90d71d6ffbddc0c7908439bd390c3a4ac956e0
S <picotech@pm.me>
2022-04-06 23:39:33 -0400

    getthetitles submit

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+    let titleList = [];
+    for (const book of array) {
+        titleList.push(book.title);
+    }
+    return titleList;
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
